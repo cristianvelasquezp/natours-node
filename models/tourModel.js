c4278a157ae196aea66acea7b53f8def179dee8d
Cristian Velasquez <cristianvelasquezp@gmail.com>
2022-05-04 10:15:13 -0500

    Create tourModel

---
+++ models/tourModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true
+  },
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  groupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  ratingAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0
+  },
+  summary: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a Image']
+  },
+  images: [String],
+  createAt: {
+    type: Date,
+    default: Date.now()
+  },
+  starDates: [Date]
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 8080;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => console.log('database connection successful'));
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('express listen');
 });
